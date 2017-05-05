cf6102cad6c7784d9c698fa88c91f23c1f18b573
Goose <josephtrop@gmail.com>
2017-05-04 21:22:56 -0700

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
      * @param  - requested character
      * @returns {*} an object character or array of carachers
      */
+    var saiyan=1
 
     function setCharacters(rqst){
         var goku ={
@@ -27,7 +28,11 @@ $(document).ready(function(){
             id     : "trunks",
             attack : 12
         }
-        var opponents = [goku,gohan,vegeta,trunks]
+        var broly ={
+            id     : "broly",
+            attack : 27
+        }
+
         switch (rqst){
             case "goku":
                 return goku
@@ -41,24 +46,14 @@ $(document).ready(function(){
             case "trunks":
                 return trunks
                 break;
-            case "opponents":
-                return opponents
+            case "broly":
+                return broly
                 break;
+
         }
     }
 
-    /**
-     *
-     * @param player
-     * @returns {contenders} -  array of opponents
-     */
 
-    function filterOpponents(player){
-        var contenders = $.grep(setCharacters("opponents"), function(n,i){
-            return (n.id !== player.id)
-        })
-        return contenders
-    }
 
     /**
      * Check if the arena is filled with players
@@ -72,30 +67,65 @@ $(document).ready(function(){
         }
         return isArenaFilled
     }
+
+    /**
+     * Check if opponent is eliminated
+     * @param health
+     * @returns {boolean}
+     */
     function opponentDefeated(health) {
         if(health <= 0){
            return true
         }
     }
 
+    /**
+     * Show logs
+     * @param player
+     * @param enemy
+     * @param attack
+     */
+    function attacklog(player,enemy, attack){
+        $("#attack-log").html("<h3>" + player + " attacked "+ enemy +" with "+ attack +" power</h3>")
+    }
+
+    function counterlog(enemy,player,defend){
+        $("#counter-attack-log").html("<h3>" + enemy + " countered  "+ player +" with counter attack of "+ defend +" power</h3>")
+    }
+
+    /**
+     * restart the game
+     */
+
+    $(".restart").on("click", function(){
+        location.reload();
+    })
 
     /**
      * set of on clicks to move the player to the arena
      */
     $("#goku").on("click",function(){
         var player = setCharacters("goku")
-        var opponents = filterOpponents(player)
 
         if ($(".player-side").children().length == 0){
             $(this).appendTo(".player-side").addClass("player")
         }else if ($(".opponent-side").children().length == 0){
             $(this).appendTo(".opponent-side").addClass("opponent")
         }
-
     })
+
+    $("#broly").on("click",function(){
+        var player = setCharacters("broly")
+        if ($(".player-side").children().length == 0){
+            $(this).appendTo(".player-side").addClass("player")
+        }else if ($(".opponent-side").children().length == 0){
+            $(this).appendTo(".opponent-side").addClass("opponent")
+        }
+    })
+
+
     $("#gohan").on("click",function(){
         var player = setCharacters("gohan")
-        var opponents = filterOpponents(player)
 
         if ($(".player-side").children().length == 0){
             $(this).appendTo(".player-side").addClass("player")
@@ -105,7 +135,6 @@ $(document).ready(function(){
     })
     $("#vegeta").on("click",function(){
         var player = setCharacters("vegeta")
-        var opponents = filterOpponents(player)
         if ($(".player-side").children().length == 0){
             $(this).appendTo(".player-side").addClass("player")
         }else  if ($(".opponent-side").children().length == 0){
@@ -115,7 +144,6 @@ $(document).ready(function(){
     })
     $("#trunks").on("click",function(){
         var player = setCharacters("trunks")
-        var opponents = filterOpponents(player)
         if ($(".player-side").children().length == 0){
             $(this).appendTo(".player-side").addClass("player")
         }else if ($(".opponent-side").children().length == 0){
@@ -125,23 +153,35 @@ $(document).ready(function(){
 
     $("button.attack").on("click", function(){
        //check first if both sides are filled with characters
+
         if(arenaIsFilled()){
-            //Get the attribute of the children of the player side
+
             var player = setCharacters($(".player-side").children().attr('id'))
             var opponent = setCharacters($(".opponent-side").children().attr('id'))
-
             var playerHealth = parseInt($(".player-side").children().text())
             var opponentHealth = parseInt($(".opponent-side").children().text())
-            $(".opponent-side div:eq(1)").html(opponentHealth-player.attack)
+            console.log(saiyan)
+            $(".opponent-side div:eq(1)").html(opponentHealth-(player.attack*saiyan))
+            console.log(opponentHealth-(player.attack*saiyan))
+            attacklog(player.id,opponent.id,(player.attack*saiyan))
 
             if(opponentDefeated(parseInt(opponentHealth-player.attack))){
                 $(".opponent-side").empty()
+                saiyan=saiyan*2
             }
             $(".player-side div:eq(1)").html(playerHealth-opponent.attack)
+            counterlog(opponent.id,player.id,opponent.attack)
+
 
             if(opponentDefeated(parseInt(playerHealth-opponent.attack))){
                 $(".player-side").empty()
+                $("#lost").html("<h1>You're defeated by " + opponent.id+" </h1>")
+                $(".attack").hide("slow")
+                $(".restart").show("slow")
             }
+
+
+
 
 
         }
